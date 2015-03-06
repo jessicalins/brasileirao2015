b9e8597fed41a2fc44c8e8e767566064486eac92
Jessica Lins <jessicalins81@gmail.com>
2015-03-05 22:59:57 -0300

    Removed button add player from initial page and small fixes

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -14,6 +14,7 @@ class PlayersController < ApplicationController
 		if @player.save
 			redirect_to team_path(@player.team)
 		else
+			params[:team_id] = @player.team_id
 			render :new  
 		end
 	end
