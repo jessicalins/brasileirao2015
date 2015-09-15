9e83d9100f184c5ecc615e60c2b2f8d14753efcf
Jessica Lins <jessicalins81@gmail.com>
2015-09-14 22:19:49 -0300

    Trying to fix github indentation

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,43 +1,43 @@
 class PlayersController < ApplicationController
-	before_action :set_player, only: [:show, :edit, :update, :destroy]
+  before_action :set_player, only: [:show, :edit, :update, :destroy]
 
-	def index
-		@players = Player.all
-	end
+  def index
+    @players = Player.all
+  end
 
-	def new 
-		@player = Player.new 
-	end
+  def new 
+    @player = Player.new 
+  end
 
-	def create
-		@player = Player.new(player_params)
-		if @player.save
-			redirect_to team_path(@player.team)
-		else
-			params[:team_id] = @player.team_id
-			render :new  
-		end
-	end
+  def create
+    @player = Player.new(player_params)
+    if @player.save
+      redirect_to team_path(@player.team)
+    else
+      params[:team_id] = @player.team_id
+      render :new  
+    end
+  end
 
-	def update
-		if @player.update(player_params)
-			redirect_to @player
-		else
-			render :edit
-		end
-	end
+  def update
+    if @player.update(player_params)
+      redirect_to @player
+    else
+      render :edit
+    end
+  end
 
-	def destroy
-		@player.destroy
-		redirect_to team_path(@player.team)
-	end
+  def destroy
+    @player.destroy
+    redirect_to team_path(@player.team)
+  end
 
-	private
-		def set_player
-			@player = Player.find(params[:id])
-		end
+  private
+    def set_player
+      @player = Player.find(params[:id])
+    end
 
-		def player_params
-			params.require(:player).permit(:name, :position, :age, :picture, :team_id)
-		end
+    def player_params
+      params.require(:player).permit(:name, :position, :age, :picture, :team_id)
+    end
 end
